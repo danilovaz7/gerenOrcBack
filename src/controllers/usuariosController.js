5236818ec7ea5e219a89b5531ba59ccb095266d8
Danilo Vaz <danilovaz06dv@gmail.com>
2025-07-04 14:30:09 -0300

    att createuser e update user

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -21,34 +21,12 @@ export function generateRandomPassword() {
 
 export async function createUser(req, res) {
   let {
-    nome,
-    id_tipo_usuario,
-    dt_nascimento,
-    rg,
-    cpf,
-    estado_civil,
-    sexo,
-    endereco,
-    num_endereco,
-    complemento,
-    cidade,
-    bairro,
-    cep,
-    naturalidade,
-    nacionalidade,
-    raca,
-    filhos,
-    telefone,
-    celular,
-    profissao,
-    local_trabalho,
-    email,
-    instagram,
-    facebook
+    nome,id_tipo_usuario,dt_nascimento,rg,cpf,estado_civil,sexo,endereco,num_endereco,complemento,cidade,bairro,
+    cep,naturalidade,nacionalidade,raca,filhos,telefone,celular,profissao,local_trabalho,email,instagram,facebook
   } = req.body;
 
   try {
-    // --- monta payload dos campos do usuário ---
+
     const payload = {
       nome,
       id_tipo_usuario: parseInt(id_tipo_usuario, 10),
@@ -78,11 +56,11 @@ export async function createUser(req, res) {
       facebook
     };
 
-    // --- gera e criptografa a senha ---
+   
     const senhaRandom = generateRandomPassword();
     const senhaHash = await bcrypt.hash(senhaRandom, 10);
 
-    // --- instancia e salva o usuário ---
+ 
     const usuario = Usuario.build({
       ...payload,
       senha: senhaHash,
@@ -90,13 +68,10 @@ export async function createUser(req, res) {
     });
     await usuario.validate();
     await usuario.save();
-
-    // --- cria registros iniciais nas tabelas filhas ---
-    // usando o modelo diretamente:
-    await UsuarioAnamnese.create({ usuario_id: usuario.id });
+    
+    await UsuarioAnamnese.create({ usuario_id: usuario.id,pressao_tipo: 'Normal', diabetico: 'NÃO SABE', prob_cardiaco: 'NÃO SABE',anemia: 'NÃO SABE',hepa: 'NÃO SABE', outra_doenca: 'NÃO SABE' });
     await UsuarioExameComplementar.create({ usuario_id: usuario.id });
 
-    // --- devolve o user criado com a senha em texto ---
     const userData = usuario.toJSON();
     return res.status(201).json({
       ...userData,
@@ -178,44 +153,19 @@ async function updateUser(req, res) {
     local_trabalho, email, instagram, facebook, senha, anamnese, examesComplementares } = req.body;
 
   const usuario = await Usuario.findByPk(id)
-  const usuarioAnamnese = await UsuarioAnamnese.findOne({ Where: { usuario_id: id } })
+  const usuarioAnamnese = await UsuarioAnamnese.findOne({ where: { usuario_id: usuario.id } });
+  const usuarioExCompl = await UsuarioExameComplementar.findOne({ where: { usuario_id: usuario.id } })
 
   if (!usuario) {
     return res.status(404).json({ error: 'Usuário não encontrado' })
   }
 
-
-  if (anamnese.comp_trat_odon !== undefined) usuarioAnamnese.comp_trat_odon = anamnese.comp_trat_odon
-  if (anamnese.comp_trat_odon_obs !== undefined) usuarioAnamnese.comp_trat_odon_obs = anamnese.comp_trat_odon_obs
-  if (anamnese.em_trat_medico !== undefined) usuarioAnamnese.em_trat_medico = anamnese.em_trat_medico
-  if (anamnese.em_trat_medico_obs !== undefined) usuarioAnamnese.em_trat_medico_obs = anamnese.em_trat_medico_obs
-  if (anamnese.transfusao !== undefined) usuarioAnamnese.transfusao = anamnese.transfusao
-  if (anamnese.transfusao_obs !== undefined) usuarioAnamnese.transfusao_obs = anamnese.transfusao_obs
-  if (anamnese.doenca_grave !== undefined) usuarioAnamnese.doenca_grave = anamnese.doenca_grave
-  if (anamnese.doenca_grave_obs !== undefined) usuarioAnamnese.doenca_grave_obs = anamnese.doenca_grave_obs
-  if (anamnese.alergia_geral !== undefined) usuarioAnamnese.alergia_geral = anamnese.alergia_geral
-  if (anamnese.alergia_geral_obs !== undefined) usuarioAnamnese.alergia_geral_obs = anamnese.alergia_geral_obs
-  if (anamnese.hospitalizado !== undefined) usuarioAnamnese.hospitalizado = anamnese.hospitalizado
-  if (anamnese.hospitalizado_obs !== undefined) usuarioAnamnese.hospitalizado_obs = anamnese.hospitalizado_obs
-  if (anamnese.submetido_cirurgia !== undefined) usuarioAnamnese.submetido_cirurgia = anamnese.submetido_cirurgia
-  if (anamnese.cirurgia_obs !== undefined) usuarioAnamnese.cirurgia_obs = anamnese.cirurgia_obs
-  if (anamnese.recebeu_anestesia !== undefined) usuarioAnamnese.recebeu_anestesia = anamnese.recebeu_anestesia
-  if (anamnese.comp_anestesia !== undefined) usuarioAnamnese.comp_anestesia = anamnese.comp_anestesia
-  if (anamnese.comp_anestesia_obs !== undefined) usuarioAnamnese.comp_anestesia_obs = anamnese.comp_anestesia_obs
-  if (anamnese.dor_dente !== undefined) usuarioAnamnese.dor_dente = anamnese.dor_dente
-  if (anamnese.dor_dente_obs !== undefined) usuarioAnamnese.dor_dente_obs = anamnese.dor_dente_obs
-  if (anamnese.protese_cardiaca !== undefined) usuarioAnamnese.protese_cardiaca = anamnese.protese_cardiaca
-  if (anamnese.protese_cardiaca_obs !== undefined) usuarioAnamnese.protese_cardiaca_obs = anamnese.protese_cardiaca_obs
-  if (anamnese.sangramento_anormal !== undefined) usuarioAnamnese.sangramento_anormal = anamnese.sangramento_anormal
-  if (anamnese.sangramento_anormal_obs !== undefined) usuarioAnamnese.sangramento_anormal_obs = anamnese.sangramento_anormal_obs
-
+  usuarioAnamnese.set(anamnese);
+  usuarioExCompl.set(examesComplementares);
 
   if (nome) usuario.nome = nome
   if (email) usuario.email = email
-  if (senha) {
-    const senhaCriptografada = await bcrypt.hash(senha, 10);
-    usuario.senha = senhaCriptografada;
-  }
+  if (senha) { const senhaCriptografada = await bcrypt.hash(senha, 10); usuario.senha = senhaCriptografada; }
   if (dt_nascimento) usuario.dt_nascimento = dt_nascimento
   if (estado_civil) usuario.estado_civil = estado_civil
   if (sexo) usuario.sexo = sexo
@@ -238,30 +188,53 @@ async function updateUser(req, res) {
   if (instagram) usuario.instagram = instagram
   if (facebook) usuario.facebook = facebook
 
+
   try {
-    await usuario.validate();
     await usuarioAnamnese.validate();
   } catch (err) {
-    return res
-      .status(400)
-      .json({ error: 'Dados inválidos: ' + err.message });
+    return res.status(400).json({ error: 'Anamnese inválida: ' + err.message });
+  }
+  try {
+    await usuario.validate();
+  } catch (err) {
+    return res.status(400).json({ error: 'usuario inválida: ' + err.message });
+  }
+  try {
+    await usuarioExCompl.validate();
+  } catch (err) {
+    return res.status(400).json({ error: 'Exames complementares inválida: ' + err.message });
   }
 
-  // 2) salvamentos
   try {
-    // se você estiver usando transação, inclua aqui os saves dentro dela
-    await usuario.save();
     await usuarioAnamnese.save();
   } catch (err) {
+    console.error('Erro ao salvar anamnese:', err);
     return res
       .status(500)
-      .json({ error: 'Erro ao salvar dados: ' + err.message });
+      .json({ error: 'Erro ao salvar anamnese: ' + err.message });
+  }
+  try {
+    await usuario.save();
+  } catch (err) {
+    console.error('Erro ao salvar anamnese:', err);
+    return res
+      .status(500)
+      .json({ error: 'Erro ao salvar usuario: ' + err.message });
+  }
+  try {
+    await usuarioExCompl.save();
+  } catch (err) {
+    console.error('Erro ao salvar exames compl:', err);
+    return res
+      .status(500)
+      .json({ error: 'Erro ao salvar exames compl: ' + err.message });
   }
 
-  // 3) envia **uma** resposta só, com tudo atualizado
   return res.json({
     usuario: usuario.toJSON(),
     anamnese: usuarioAnamnese.toJSON(),
+    examesComplementares: usuarioExCompl.toJSON(),
+
   });
 }
 
