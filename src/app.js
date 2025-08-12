6009cd67b05d248a1e52b46a3b392223498f4692
Danilo Vaz <danilovaz06dv@gmail.com>
2025-08-12 12:05:28 -0300

    a

--- src/app.js
+++ src/app.js
@@ -8,17 +8,9 @@ import router from './routes/router.js';
 
 dotenv.config();
 console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL)
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true,
-};
-
 const app = express();
 
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
