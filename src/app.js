8205885c4e447e5c8ed6c318cf0ac64c23b5e4ad
Danilo Vaz <danilovaz06dv@gmail.com>
2025-08-12 12:12:08 -0300

    b

--- src/app.js
+++ src/app.js
@@ -10,7 +10,18 @@ dotenv.config();
 console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL)
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'https://scso0gcwswksoow80k004g8w.69.62.99.208.sslip.io',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true,
+};
+
+// Usar CORS com opções e habilitar preflight
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
